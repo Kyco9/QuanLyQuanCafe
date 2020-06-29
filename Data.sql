a9e8f5bf65cec8ce37fce922dbb69229fa4fbb00
Kyco9 <quangtrungben10@gmail.com>
2020-06-29 17:46:54 +0700

    Tao csdl

---
+++ Data.sql
@@ -0,0 +1,28 @@
+﻿CREATE DATABASE QuanLyQuanCafe
+GO
+
+USE QuanLyQuanCafe
+GO
+
+--Các bảng cần có
+-- Food
+-- Table
+-- FoodCategory
+-- Account
+-- Bill
+-- BillInfo
+
+CREATE TABLE TableFood
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100),
+	status NVARCHAR(100), --Trống || Có người
+)
+GO
+
+CREATE TABLE Account
+(
+	id INT IDENTITY PRIMARY KEY,
+
+)
+GO
