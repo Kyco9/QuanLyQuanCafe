2b4cf607dd8dd58173f5b560baea5f8ca41462f9
Kyco9 <quangtrungben10@gmail.com>
2020-06-29 18:34:02 +0700

    Complete SQL

--- Data.sql
+++ Data.sql
@@ -15,14 +15,58 @@ GO
 CREATE TABLE TableFood
 (
 	id INT IDENTITY PRIMARY KEY,
-	name NVARCHAR(100),
-	status NVARCHAR(100), --Trống || Có người
+	name NVARCHAR(100) NOT NULL DEFAULT N'Bàn chưa có tên',
+	status NVARCHAR(100) NOT NULL DEFAULT N'Trống'	-- Trống || Có người
 )
 GO
 
 CREATE TABLE Account
 (
-	id INT IDENTITY PRIMARY KEY,
+	UserName NVARCHAR(100) PRIMARY KEY,	
+	DisplayName NVARCHAR(100) NOT NULL DEFAULT N'Khách hàng',
+	PassWord NVARCHAR(1000) NOT NULL DEFAULT 0,
+	Type INT NOT NULL  DEFAULT 0 -- 1: admin && 0: staff
+)
+GO
 
+CREATE TABLE FoodCategory
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'Chưa đặt tên'
+)
+GO
+
+CREATE TABLE Food
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'Chưa đặt tên',
+	idCategory INT NOT NULL,
+	price FLOAT NOT NULL DEFAULT 0
+	
+	FOREIGN KEY (idCategory) REFERENCES dbo.FoodCategory(id)
+)
+GO
+
+CREATE TABLE Bill
+(
+	id INT IDENTITY PRIMARY KEY,
+	DateCheckIn DATE NOT NULL DEFAULT GETDATE(),
+	DateCheckOut DATE,
+	idTable INT NOT NULL,
+	status INT NOT NULL DEFAULT 0 -- 1: đã thanh toán && 0: chưa thanh toán
+	
+	FOREIGN KEY (idTable) REFERENCES dbo.TableFood(id)
+)
+GO
+
+CREATE TABLE BillInfo
+(
+	id INT IDENTITY PRIMARY KEY,
+	idBill INT NOT NULL,
+	idFood INT NOT NULL,
+	count INT NOT NULL DEFAULT 0
+	
+	FOREIGN KEY (idBill) REFERENCES dbo.Bill(id),
+	FOREIGN KEY (idFood) REFERENCES dbo.Food(id)
 )
 GO
